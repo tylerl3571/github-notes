9a38f49fdf48fc9783ca3644441d7201924dbed2
tylerl3571 <114585709+tylerl3571@users.noreply.github.com>
2022-11-29 16:01:10 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let ownerName = "Tyler Lee"; // update the part BETWEEN the "quotes"
+let userName = "tylerl3571"; // same here
+
+document.querySelectorAll(".owner-name").forEach((e) => {
+    e.innerHTML = ownerName;
+});
+
+document.querySelector("#github").href = "http://github.com/" + userName;
+document.querySelector("#fork").href = "http://github.com/" + userName + "/" + userName + ".github.io";
